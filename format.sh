36e1d2f2283d1c0404f96e1a8a10bbff4269305a
bwang1008 <bwang1008@gmail.com>
2022-11-27 02:23:15 -0600

    Updated documentation of diagnostic, format.sh

--- format.sh
+++ format.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/bash
+
+# usage:
+# $ ./format.sh <file_name>
+# Example:
+# $ ./format.sh TMCompiler/src/unittesting/unittesting.hpp
+
+# .clang-format file generated through online sources, then pasted to .clang-format
 clang-format -i --style=file $1
