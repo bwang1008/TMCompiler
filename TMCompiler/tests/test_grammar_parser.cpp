eefebe619cead0b10aaf1ac13f50631f62d42a65
bwang1008 <bwang1008@gmail.com>
2023-03-19 16:13:05 -0500

    Add skeleton grammar tests

---
+++ TMCompiler/tests/test_grammar_parser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <TMCompiler/compiler/front_end/read_grammar.hpp>   // read_grammar()
+#include <TMCompiler/utils/unittesting/unittests.hpp>
+
+// g++ -std=c++11 -I. TMCompiler/tests/test_grammar_parser.cpp
+
+TEST_CASE(test0) {
+	Grammar grammar = read_grammar("data/example_grammar.bnf")
+
+	ASSERT(0 == 0);
+}
+
+
