890ab33e33a1e6ada2e4496f748bb5dde637f947
bwang1008 <bwang1008@gmail.com>
2023-03-19 19:07:17 -0500

    Update formatter and linter

--- format.sh
+++ format.sh
@@ -6,4 +6,4 @@
 # $ ./format.sh TMCompiler/src/unittesting/unittesting.hpp
 
 # .clang-format file generated through online sources, then pasted to .clang-format
-clang-format -i --style=file $1
+clang-format -i --style=file "$1"
