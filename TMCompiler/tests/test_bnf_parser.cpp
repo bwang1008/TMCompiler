db8665ee6c752d4c3c60231e905c3ef890f80b69
bwang1008 <bwang1008@gmail.com>
2023-03-26 03:31:39 -0500

    Update BNF parser tests

--- TMCompiler/tests/test_bnf_parser.cpp
+++ TMCompiler/tests/test_bnf_parser.cpp
@@ -1,4 +1,7 @@
 #include <fstream>
+#include <map>
+#include <string>
+#include <vector>
 
 #include <TMCompiler/utils/unittesting/unittests.hpp>
 #include <TMCompiler/compiler/models/bnf_parser.hpp>
@@ -8,4 +11,58 @@ TEST_CASE(test0) {
 	Rules rules = BnfParser::parse_rules(input_bnf);
 
 	ASSERT(rules.size() == 4);
+
+	int index = 0;
+	for(std::map<std::string, std::vector<std::vector<Symbol> > >::iterator it = rules.begin(); it != rules.end(); ++it) {
+		const std::string lhs = it->first;
+		ReplacementAlternatives rhs = it->second;
+		if(index == 0) {
+			ASSERT(lhs == "M");
+			ASSERT(rhs.size() == 2);
+
+			ASSERT(rhs[0].size() == 3);
+			ASSERT(rhs[0][0].get_name() == "M");
+			ASSERT(rhs[0][1].get_name() == "*");
+			ASSERT(rhs[0][2].get_name() == "T");
+
+			ASSERT(rhs[1].size() == 1);
+			ASSERT(rhs[1][0].get_name() == "T");
+		}
+		else if(index == 1) {
+			ASSERT(lhs == "P");
+			ASSERT(rhs.size() == 1);
+			ASSERT(rhs[0].size() == 1);
+			ASSERT(rhs[0][0].get_name() == "S");
+		}
+		else if(index == 2) {
+			ASSERT(lhs == "S");
+			ASSERT(rhs.size() == 2);
+
+			ASSERT(rhs[0].size() == 3);
+			ASSERT(rhs[0][0].get_name() == "S");
+			ASSERT(rhs[0][1].get_name() == "+");
+			ASSERT(rhs[0][2].get_name() == "M");
+
+			ASSERT(rhs[1].size() == 1);
+			ASSERT(rhs[1][0].get_name() == "M");
+		}
+		else if(index == 3) {
+			ASSERT(lhs == "T");
+			ASSERT(rhs.size() == 4);
+
+			ASSERT(rhs[0].size() == 1);
+			ASSERT(rhs[0][0].get_name() == "1");
+
+			ASSERT(rhs[1].size() == 1);
+			ASSERT(rhs[1][0].get_name() == "2");
+
+			ASSERT(rhs[2].size() == 1);
+			ASSERT(rhs[2][0].get_name() == "3");
+
+			ASSERT(rhs[3].size() == 1);
+			ASSERT(rhs[3][0].get_name() == "4");
+		}
+
+		++index;
+	}
 }
