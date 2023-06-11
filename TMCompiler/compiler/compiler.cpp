cbb59859dc1eb4cf6e01eaf98b05f6213fb73f29
bwang1008 <bwang1008@gmail.com>
2023-06-10 23:23:25 -0500

    Skeleton of compiler

--- TMCompiler/compiler/compiler.cpp
+++ TMCompiler/compiler/compiler.cpp
@@ -1,3 +1,14 @@
 #include "compiler.hpp"
 
-#include <TMCompiler/compiler/compiler.hpp>
+#include <TMCompiler/compiler/models/grammar.hpp>
+
+class Compiler {
+
+	public:
+		Compiler();
+		void compile() const;
+
+	private:
+		Grammar lexical_grammar;
+		Grammar syntactical_grammar;
+};
