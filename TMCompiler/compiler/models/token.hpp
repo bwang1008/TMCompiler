8f40f79f517723ab6d6df869f4482fa2d0139fec
bwang1008 <bwang1008@gmail.com>
2023-08-09 21:56:09 -0500

    Comments to token.hpp

--- TMCompiler/compiler/models/token.hpp
+++ TMCompiler/compiler/models/token.hpp
@@ -8,8 +8,8 @@
 #include <string>	// std::string
 
 struct Token {
-	std::string type;
-	std::string value;
+	std::string type;	// ex: identifier, keyword
+	std::string value;  // ex: "my_name", "int", "return", "78"
 	std::size_t program_line_number;
 	std::size_t start_position_of_token_in_program_line;
 };
