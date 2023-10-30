809bdcd3878411322fd2bc6064a621a13876537b
bwang1008 <bwang1008@gmail.com>
2023-10-29 23:17:33 -0500

    Add tests for lexer

---
+++ TMCompiler/tests/test_lexer.cpp
@@ -0,0 +1,97 @@
+#include <string>  // std::string
+#include <vector>  // std::vector
+
+#include <TMCompiler/compiler/lexer/lexer.hpp>		   // Lexer
+#include <TMCompiler/compiler/models/token.hpp>		   // Token
+#include <TMCompiler/utils/unittesting/unittests.hpp>  // TEST_CASE, ASSERT
+
+TEST_CASE(test_lexer_comment_0) {
+	Lexer lexer("TMCompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/**/";
+
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
+
+TEST_CASE(test_lexer_comment_1) {
+	Lexer lexer("TMCompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/* */";
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
+
+TEST_CASE(test_lexer_comment_2) {
+	Lexer lexer("TMCompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/***/";
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
+
+TEST_CASE(test_lexer_comment_3) {
+	Lexer lexer("TMCompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/**********/";
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
+
+TEST_CASE(test_lexer_comment_4) {
+	Lexer lexer("tmcompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/*/*/";
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
+
+TEST_CASE(test_lexer_comment_5) {
+	Lexer lexer("TMCompiler/config/regex_lexical_grammar.bnf");
+
+	std::string program_text = "/*/////////////*/";
+	lexer.set_text(program_text);
+	std::vector<Token> tokens;
+
+	while(lexer.has_next_token()) {
+		tokens.push_back(lexer.get_next_token());
+	}
+
+	ASSERT(tokens.size() == 1);
+	ASSERT(tokens[0].type == "block-comment");
+}
