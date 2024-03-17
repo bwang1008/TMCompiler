a6df377aeb2f3f09cb72754708e0a76a7dabfea4
bwang1008 <bwang1008@gmail.com>
2024-03-17 18:09:33 -0500

    Use Catch2 sections in test_compiler

--- TMCompiler/tests/test_compiler.cpp
+++ TMCompiler/tests/test_compiler.cpp
@@ -9,177 +9,65 @@ TEST_CASE("program_text_0") {
 	logger.set_level("NONE");
 
 	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"void foo() {}"};
+	std::string program_text;
+
+	SECTION("program_text_void") {
+		program_text = "void foo() {}";
+	}
+	SECTION("program_text_1") {
+		program_text = "int compute(int y) { return 5; }";
+	}
+	SECTION("program_text_check_0") {
+		program_text = "int compute(int y) { return 0; }";
+	}
+	SECTION("program_text_check_negative") {
+		program_text = "int compute(int y) { return -3; }";
+	}
+	SECTION("program_text_check_negative_basic_statement") {
+		program_text = "void foo() { int x = -1; }";
+	}
+	SECTION("program_two_functions") {
+		program_text = "void foo() {} int compute(int y) { return 5; }";
+	}
+	SECTION("basic_statement") {
+		program_text = "void foo() { int sum = 0; }";
+	}
+	SECTION("two_basic_statements") {
+		program_text = "void foo() { int sum = 0; int extra = -25; }";
+	}
+	SECTION("compound_operator") {
+		program_text = "void foo() { int sum = 0; sum += 5;}";
+	}
+	SECTION("for_loop") {
+		program_text =
+			"int foo() { int sum = 0; for(int i = 0; i < 10; i += 1) { sum += "
+			"i; } return sum; }";
+	}
+	SECTION("if_statement") {
+		program_text = "int foo() { if(true) { return 1; } return -1; }";
+	}
+	SECTION("if_statement_no_bracket") {
+		program_text = "int foo() { if(true) return 1; return -1; }";
+	}
+	SECTION("if_else_statement") {
+		program_text =
+			"int foo() { if(true) { return 1; } else { return -1; } }";
+	}
+	SECTION("if_else_if_else_statement") {
+		program_text =
+			"bool check() { return true; } int foo() { if(check()) { return 1; "
+			"} return -1; }";
+	}
+	SECTION("while_statement") {
+		program_text = "int foo() { while(true) { return 1; }}";
+	}
+	SECTION("newline_and_tabs") {
+		program_text = "int main() {\n\treturn 0;}";
+	}
+	SECTION("method_invocation") {
+		program_text = "void foo() {}  void main() { foo(); }";
+	}
+
 	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("program_text_1") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"int compute(int y) { return 5; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("program_text_check_0") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"int compute(int y) { return 0; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("program_text_check_negative") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"int compute(int y) { return -3; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("program_text_check_negative_basic_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"void foo() { int x = -1; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("program_two_functions") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"void foo() {} int compute(int y) { return 5; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("basic_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"void foo() { int sum = 0; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("two_basic_statements") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"void foo() { int sum = 0; int extra = -25; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("compound_operator") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"void foo() { int sum = 0; sum += 5;}"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("for_loop") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"int foo() { int sum = 0; for(int i = 0; i < 10; i += 1) { sum += i; } "
-		"return sum; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("if_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"int foo() { if(true) { return 1; } return -1; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("if_statement_no_bracket") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"int foo() { if(true) return 1; return -1; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("if_else_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"int foo() { if(true) { return 1; } else { return -1; } }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("if_else_if_else_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{
-		"bool check() { return true; } int foo() { if(check()) { return 1; } "
-		"return -1; }"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("while_statement") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"int foo() { while(true) { return 1; }}"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("newline_and_tabs") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"int main() {\n\treturn 0;}"};
-	compiler.compile_text(program_text);
-
-	REQUIRE(1 == 1);
-}
-
-TEST_CASE("method_invocation") {
-	logger.set_level("NONE");
-
-	Compiler compiler("TMCompiler/config/language.toml");
-	const std::string program_text{"void foo() {}  void main() { foo(); }"};
-	compiler.compile_text(program_text);
-
 	REQUIRE(1 == 1);
 }
